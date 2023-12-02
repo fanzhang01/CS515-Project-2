a0d48ba9f4c3ad693a787244c7f1301c8e9807bf
fanzhang01 <fzhang32@stevens.edu>
2023-12-02 18:42:51 -0500

    fourth commit

--- adventure.py
+++ adventure.py
@@ -1,184 +1,165 @@
-
 import json
 import sys
-import msvcrt
+
 
 def load_map(filename):
     with open(filename, 'r') as file:
         return json.load(file)
 
 
-class Game:
-    def __init__(self, mapfile):
-        self.rooms = load_map(mapfile)
-        self.current_room = 0
-        self.inventory = []
+def display_room(rooms, current_room):
+    room = rooms[current_room]
+    print(f"> {room['name']}\n")
+    print(f"{room['desc']}\n")
+    if 'items' in room:
+        items = ", ".join(room['items'])
+        print(f"Items: {items}\n")
+    exits = " ".join(room['exits'].keys())
+    print(f"Exits: {exits}\n")
 
 
-    def display_room(self):
-        room = self.rooms[self.current_room]
-        print(f"> {room['name']}\n")
-        print(f"{room['desc']}\n")
-        if 'items' in room:
-            items = ", ".join(room['items'])
-            print(f"Items: {items}\n")
-        exits = " ".join(room['exits'].keys())
-        print(f"Exits: {exits}\n")
+def resolve_abbreviation(input, options, verb):
+    if input in options:
+        return input, False  # False indicates no ambiguity
+
+    # If no exact match, check for prefix matches
+    matches = [option for option in options if option.startswith(input)]
+    if len(matches) == 1:
+        return matches[0], False  # Single match, no ambiguity
+    elif len(matches) > 1:
+        formatted_matches = ' or '.join(matches)
+        print(f"Did you want to {verb} {formatted_matches}?")
+        return matches, True  # Multiple matches, ambiguity present
+    else:
+        return None, False  # No matches
 
 
-    def move(self, direction):
-        current_exits = self.rooms[self.current_room]['exits']
+def go_direction(command, rooms, current_room):
+    parts = command.split(maxsplit=1)
+    if len(parts) == 1:
+        print("Sorry, you need to 'go' somewhere.")
+        return current_room
+
+    direction = parts[1]
+    resolved_direction, ambiguous = resolve_abbreviation(direction, rooms[current_room]['exits'], "go")
+
+    if not ambiguous and resolved_direction:
+        current_room = rooms[current_room]['exits'][resolved_direction]
+        print(f"You go {resolved_direction}.\n")
+        display_room(rooms, current_room)  # Display new room details
+    elif resolved_direction is None:
+        print(f"There's no way to go {direction}.")
+
+    return current_room
 
 
-        if direction in current_exits:
-            self.current_room = current_exits[direction]
-            print(f"You go {direction}.\n")
-            self.display_room()
-            return
+def get_item(command, rooms, current_room, inventory):
+    parts = command.split(maxsplit=1)
+    if len(parts) == 1:
+        print("Sorry, you need to 'get' something.")
+        return current_room, inventory
+
+    item = parts[1]
+    if 'items' in rooms[current_room]:
+        resolved_item, ambiguous = resolve_abbreviation(item, rooms[current_room]['items'], "get")
+        if not ambiguous and resolved_item:
+            inventory.append(resolved_item)
+            rooms[current_room]['items'].remove(resolved_item)
+            print(f"You pick up the {resolved_item}.")
+        elif resolved_item is None:
+            print(f"There's no {item} to get.")
+    else:
+        print("There are no items to get in this room.")
+
+    return current_room, inventory
 
 
-        matched_exits = [exit for exit in current_exits if exit.startswith(direction)]
-        if len(matched_exits) == 1:
-            self.current_room = current_exits[matched_exits[0]]
-            print(f"You go {matched_exits[0]}.\n")
-            self.display_room()
-        elif len(matched_exits) > 1:
-            print(f"Did you want to go {' or '.join(matched_exits)}?")
+def drop_item(command, current_room, inventory):
+    # Implement drop_item functionality
+    pass
+
+
+def inventory_items(current_room, inventory):
+    show_inventory(inventory)
+
+
+def quit_game():
+    print("Goodbye!")
+    sys.exit(0)
+
+
+def help_command():
+    # Implement help_command functionality
+    pass
+
+
+def show_inventory(inventory):
+    if inventory:
+        print("Inventory:\n  " + "\n  ".join(inventory))
+    else:
+        print("You're not carrying anything.")
+
+
+if len(sys.argv) < 2:
+    print("Usage: python adventure.py [map filename]")
+    sys.exit(1)
+
+mapfile = sys.argv[1]
+rooms = load_map(mapfile)
+current_room = 0
+inventory = []
+
+display_room(rooms, current_room)
+
+valid_commands = ["go", "get", "drop", "look", "inventory", "quit", "help"]
+directions = ["north", "south", "east", "west", "northeast", "northwest", "southeast", "southwest"]
+direction_abbreviations = {
+    'n': 'north',
+    's': 'south',
+    'e': 'east',
+    'w': 'west',
+    'ne': 'northeast',
+    'nw': 'northwest',
+    'se': 'southeast',
+    'sw': 'southwest'
+}
+
+while True:
+    try:
+        raw_input = input('What would you like to do? ').lower()
+    except EOFError:
+        print()
+        print("Use 'quit' to exit.")
+        continue
+
+    parts = raw_input.split(maxsplit=1)
+    command_verb = parts[0]
+    rest_of_command = parts[1] if len(parts) > 1 else ""
+
+    # Check for direction abbreviations
+    if command_verb in direction_abbreviations:
+        command_verb = direction_abbreviations[command_verb]
+
+    if command_verb in directions:  # Check if it's a direction
+        current_room = go_direction(f"go {command_verb}", rooms, current_room)
+    else:
+        resolved_verb, ambiguous = resolve_abbreviation(command_verb, valid_commands, "do")
+        if ambiguous or resolved_verb is None:
+            continue  # Skip the loop if command is ambiguous or invalid
+
+        if resolved_verb == 'look':
+            display_room(rooms, current_room)
+        elif resolved_verb == 'go':
+            current_room = go_direction(f"{resolved_verb} {rest_of_command}", rooms, current_room)
+        elif resolved_verb == 'get':
+            current_room, inventory = get_item(f"{resolved_verb} {rest_of_command}", rooms, current_room, inventory)
+        elif resolved_verb == 'drop':
+            current_room, inventory = drop_item(f"{resolved_verb} {rest_of_command}", current_room, inventory)
+        elif resolved_verb == 'inventory':
+            inventory_items(current_room, inventory)
+        elif resolved_verb == 'quit':
+            quit_game()
+        elif resolved_verb == 'help':
+            help_command()
         else:
-            print(f"There's no way to go {direction}.")
-
-
-    def take_item(self, item_input):
-        room = self.rooms[self.current_room]
-        if 'items' in room:
-
-            matched_items = [item for item in room['items'] if item_input in item]
-
-            if len(matched_items) == 1:
-                item = matched_items[0]
-                self.inventory.append(item)
-                room['items'].remove(item)
-                print(f"You pick up the {item}.")
-            elif len(matched_items) > 1:
-
-                matches_formatted = ', '.join(matched_items[:-1]) + ' or ' + matched_items[-1]
-                print(f"Did you want to get the {matches_formatted}?")
-            else:
-                print(f"There's no {item_input} to get.")
-        else:
-            print("There are no items to get in this room.")
-
-    def show_inventory(self):
-        if self.inventory:
-            print("Inventory:\n  " + "\n  ".join(self.inventory))
-        else:
-            print("You're not carrying anything.")
-
-
-    def resolve_abbreviation(self, abbreviation, options):
-        matches = [option for option in options if option.startswith(abbreviation)]
-        if len(matches) == 1:
-            return matches[0]
-        elif len(matches) > 1:
-            print(f"Did you mean one of these? {', '.join(matches)}")
-            return None
-        else:
-            print("No matching command found.")
-            return None
-
-
-    def parse_command(self, command):
-        words = command.lower().split()
-        if not words:
-            return
-        verb = words[0]
-
-        direction_abbreviations = {
-            'nw': 'northwest',
-            'ne': 'northeast',
-            'sw': 'southwest',
-            'se': 'southeast'
-        }
-        single_letter_directions = {
-            'n': 'north',
-            's': 'south',
-            'e': 'east',
-            'w': 'west'
-        }
-
-        if verb in direction_abbreviations:
-            if direction_abbreviations[verb] in self.rooms[self.current_room]['exits']:
-                self.move(direction_abbreviations[verb])
-            else:
-                print(f"There's no way to go {direction_abbreviations[verb]}.")
-            return
-
-        if len(verb) == 1 and verb in single_letter_directions:
-            direction = single_letter_directions[verb]
-            if direction in self.rooms[self.current_room]['exits']:
-                self.move(direction)
-            else:
-                print(f"There's no way to go {direction}.")
-        else:
-            if verb == 'go':
-                if len(words) > 1:
-                    self.move(' '.join(words[1:]))
-                else:
-                    print("Sorry, you need to 'go' somewhere.")
-
-            elif verb == 'get':
-                if len(words) > 1:
-                    self.take_item(' '.join(words[1:]))
-                else:
-                    print("Sorry, you need to 'get' something.")
-            elif verb == 'look':
-                self.display_room()
-
-            elif verb in ['inventory', 'inv']:
-                self.show_inventory()
-
-            elif verb == 'quit':
-                print("Goodbye!")
-                sys.exit(0)
-
-            else:
-                print("Invalid command.")
-
-    def play(self):
-        self.display_room()
-        while True:
-            try:
-                print("What would you like to do? ", end='', flush=True)
-                command = ""
-                while True:
-                    if msvcrt.kbhit():
-                        char = msvcrt.getch()
-                        # Check if it's Ctrl+D (ASCII 4)
-                        if char == b'\x04':
-                            print("^D")
-                            print("Use 'quit' to exit.")
-                            break
-
-                        if char in [b'\r', b'\n']:
-                            print()
-                            break
-                        command += char.decode()
-            except KeyboardInterrupt:
-                raise
-            else:
-                if command.strip():
-                    self.parse_command(command.strip())
-
-
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python adventure.py [map filename]")
-        sys.exit(1)
-
-    mapfile = sys.argv[1]
-    game = Game(mapfile)
-    game.play()
-
-
-if __name__ == "__main__":
-    main()
+            print("Invalid command.")
